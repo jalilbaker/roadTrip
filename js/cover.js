136ad029ac193b9326989ed3cb7ac7cc76550997
Samantha <lovelychick1111@yahoo.com>
2018-01-22 12:03:45 -0500

    add background images and card boxes for each attractration

---
+++ js/cover.js
@@ -0,0 +1,13 @@
+//silence audio
+var video = document.getElementById('player');
+video.volume = 0.0;
+
+// very simple toggle
+// function clicked () {
+//   var target =  document.getElementById("overlay").className;
+//   if (target == "bg") {
+//     document.getElementById("overlay").className = "no-bg";
+//   } else {
+//     document.getElementById("overlay").className = "bg";
+//   }
+// };
